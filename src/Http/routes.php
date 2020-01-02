9e7e2ea303f30934c62fa2f87bf7e27e5ad72d90
Richard Mura <richard@yegobox.com>
2020-01-02 18:26:44 +0200

    assign web middleware for package to access session on routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -5,32 +5,31 @@ use Illuminate\Support\Facades\Mail;
 use \Yegobox\Referral\Mail\ReferralMail;
 
 Route::get('generate_referral_code',function (){
-    $model =\Yegobox\Referral\Models\ReferralCode::where('owner_id',Auth::id())->first();
+
+    $model =\Yegobox\Referral\Models\ReferralCode::where('owner_id',request()->user()->id)->first();
     if($model){
         return env('APP_URL').'/'.config('referral.registration_end_point').'?referral_code='.$model->code;
     }
    $result = \Yegobox\Referral\Models\ReferralCode::create([
        'code'=>random_int(1000,5000)+ rand(10,20), //can be imploved!
-       'owner_id'=>Auth::id()
+       'owner_id'=>request()->user()->id
    ]) ;
 
     return env('APP_URL').'/'.config('referral.registration_end_point').'?referral_code='.$result->code;
-});
+})->middleware('web');
 Route::get('referrals',function(){
-    return \Yegobox\Referral\Models\Referral::where('owner_id',Auth::id())->first();
-});
+    return \Yegobox\Referral\Models\Referral::where('owner_id',request()->user()->id)->first();
+})->middleware('web');;
 Route::post('emails',function(){
    $arr = explode(",",request()->get('emails'));
    $clean_arr=[];
    foreach($arr as $single){
     if(!in_array($single,$clean_arr)){
         $clean_arr[]= $single;
-        //Auth::id()
-        $model =\Yegobox\Referral\Models\ReferralCode::where('owner_id',Auth::id())->first();
-
-        Log::debug($single);
-        // if(EM)
-        Mail::to($single)->send(new ReferralMail(config('referral.registration_end_point').'?referral_code='.$model->code));
+        $model =\Yegobox\Referral\Models\ReferralCode::where('owner_id',request()->user()->id)->first();
+        if(filter_var($single,FILTER_VALIDATE_EMAIL)){
+            Mail::to($single)->send(new ReferralMail(config('referral.registration_end_point').'?referral_code='.$model->code));
+        }
     }
    }
-});
+})->middleware('web');
